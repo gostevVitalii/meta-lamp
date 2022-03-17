933ca29949534d2e0f5e4f89495407bbbbb98e45
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2022-03-17 14:30:14 +0300

    Настроил Webpack

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true
+    },
+    resolve: {
+        // extensions: ['js', 'svg'],
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin(
+            {
+                template: './index.pug',
+                inject: 'body',
+                // minify: {
+                //     collapseWhitespace: isProd,
+
+                // }
+            }
+        ),
+        // new CopyWebpackPlugin([
+
+        // ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9200,
+    }
+}
